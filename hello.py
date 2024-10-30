1af3202fdde3454acbe6ab9961c09457ecd75bd9
Md. Al-Amin Kabir <alaminkabir789@gmail.com>
2024-10-30 23:10:35 +0600

    ch2

---
+++ hello.py
@@ -0,0 +1,8 @@
+name = "Alamin Kabir"
+age = 26
+id = "1810674142"
+
+print 
+"""
+multi line comment 
+"""
---
+++ inputandSum.py
@@ -0,0 +1,7 @@
+# Write a program to input 2 numbers & print their sum
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: "))
+
+sum = number1 + number2
+
+print("Sum of the two number is: ", sum)
