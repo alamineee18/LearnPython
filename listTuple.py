dbc8bca5280a50bc81339eeb8091a770a69c6762
Md. Al-Amin Kabir <alaminkabir789@gmail.com>
2024-11-18 22:56:18 +0600

    update

---
+++ listTuple.py
@@ -0,0 +1,9 @@
+mak1 = 94.4
+mak2 = 87
+mak3 = 95.2
+mak4 = 45.1
+
+marks = [94.4, 87, 95.2, 45.1]
+print(marks[2])
+print(type(marks))
+print(len(marks))
---
+++ whileLoop.py
@@ -0,0 +1,5 @@
+i = 1
+while i<10:
+    print("This is: ", i)
+    i+=1
+
